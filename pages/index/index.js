afb9f2e4e505d6fcd36783a9824831d3a64a69a5
Virgo9 <1915264421@qq.com>
2018-04-08 16:16:47 +0800

    init miniExtend

---
+++ pages/index/index.js
@@ -0,0 +1,41 @@
+//index.js
+//获取应用实例
+var app = getApp();
+Page({
+  data: {
+    userInfo: {},
+    imgalist: ['../../image/jcjy.jpeg',
+               '../../image/wx.jpg']
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    });
+  },
+  onLoad: function () {
+    var that = this;
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      });
+    });
+  },
+  phoneCall: function (e) {
+    wx.makePhoneCall({
+      phoneNumber: e.currentTarget.dataset.replyPhone,
+      success: function () {
+        console.log("成功拨打电话");
+      },
+    })
+  },
+  previewImage: function (e) {
+    var current=e.target.dataset.src;
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接
+      urls: this.data.imgalist // 需要预览的图片http链接列表
+    })
+  }
+});
